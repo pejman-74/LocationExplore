5fe92d1b94c51dcbd32d32b1ed09c08d23cdb7ad
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-07-29 01:16:30 +0430

    Type converter were added to database.

--- app/src/main/java/com/locationexplorer/data/database/AppDatabase.kt
+++ app/src/main/java/com/locationexplorer/data/database/AppDatabase.kt
@@ -2,12 +2,15 @@ package com.locationexplorer.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.locationexplorer.data.database.converter.StringListTypeConverter
 import com.locationexplorer.data.database.dao.LocationDao
 import com.locationexplorer.data.database.dao.VenueDao
 import com.locationexplorer.data.model.response.Location
 import com.locationexplorer.data.model.response.Venue
 
 @Database(entities = [Venue::class, Location::class], version = 1)
+@TypeConverters(StringListTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun venueDao(): VenueDao
