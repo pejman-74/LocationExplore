16a97de6d0d4e5eab072f236f0651b2f7fcb6bb8
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-07-29 00:09:34 +0430

    DatabaseModuleTest.kt were added.

---
+++ app/src/androidTest/java/com/locationexplorer/di/DatabaseModuleTest.kt
@@ -0,0 +1,22 @@
+package com.locationexplorer.di
+
+import android.content.Context
+import androidx.room.Room
+import com.locationexplorer.data.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@TestInstallIn(components = [SingletonComponent::class], replaces = [DatabaseModule::class])
+@Module
+object DatabaseModuleTest {
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase =
+        Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).allowMainThreadQueries()
+            .build()
+}
+
