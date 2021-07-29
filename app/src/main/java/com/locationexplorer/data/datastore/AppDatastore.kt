46b1d440f4cb542a1254fbcf8f081761abbaa678
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-07-29 17:52:59 +0430

    AppDatastore store added

---
+++ app/src/main/java/com/locationexplorer/data/datastore/AppDatastore.kt
@@ -0,0 +1,10 @@
+package com.locationexplorer.data.datastore
+
+import com.locationexplorer.data.model.share.SimpleLocation
+
+interface AppDatastore {
+    suspend fun setUserLastLocation(simpleLocation: SimpleLocation)
+    suspend fun getUserLastLocation(): SimpleLocation
+    suspend fun setLastUpdateTime(time: Long)
+    suspend fun getLastUpdateTime(): Long
+}
