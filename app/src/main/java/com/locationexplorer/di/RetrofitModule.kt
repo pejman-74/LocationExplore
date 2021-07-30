0c7c112110136082b1c3fe8b93492cb4bac8aab8
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-07-30 18:31:12 +0430

    NetworkConnectionInterceptor provided.

--- app/src/main/java/com/locationexplorer/di/RetrofitModule.kt
+++ app/src/main/java/com/locationexplorer/di/RetrofitModule.kt
@@ -1,13 +1,20 @@
 package com.locationexplorer.di
 
+import android.content.Context
 import com.locationexplorer.BASE_URL
+import com.locationexplorer.util.network.ConnectionChecker
+import com.locationexplorer.util.network.ConnectionCheckerImpl
+import com.locationexplorer.util.network.NetworkConnectionInterceptor
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -16,8 +23,28 @@ object RetrofitModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(mosh: Moshi): Retrofit =
+    fun provideNetworkChecker(@ApplicationContext context: Context): ConnectionChecker =
+        ConnectionCheckerImpl(context)
+
+    @Singleton
+    @Provides
+    fun provideNetworkConnectionInterceptor(connectionChecker: ConnectionChecker): NetworkConnectionInterceptor =
+        NetworkConnectionInterceptor(connectionChecker)
+
+    @Provides
+    @Singleton
+    @Named("NetworkConnectionClient")
+    internal fun provideNetworkConnectionInterceptorClient(networkConnectionInterceptor: NetworkConnectionInterceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(networkConnectionInterceptor).build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        mosh: Moshi,
+        @Named("NetworkConnectionClient") networkConnectionInterceptorClient: OkHttpClient
+    ): Retrofit =
         Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(MoshiConverterFactory.create(mosh))
+            .client(networkConnectionInterceptorClient)
             .build()
 
 }
