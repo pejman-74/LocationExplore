229b7e7377b6a263d3ffad51667cfe6d89187fd3
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-08-02 16:19:07 +0430

    Repository provided.

---
+++ app/src/main/java/com/locationexplorer/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.locationexplorer.di
+
+import com.locationexplorer.data.api.ExploreApi
+import com.locationexplorer.data.database.AppDatabase
+import com.locationexplorer.data.datastore.AppDatastore
+import com.locationexplorer.data.holder.IndexHolder
+import com.locationexplorer.data.repository.Repository
+import com.locationexplorer.data.repository.RepositoryImpl
+import com.locationexplorer.util.network.ConnectionChecker
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideRepository(
+        exploreApi: ExploreApi,
+        appDatabase: AppDatabase,
+        appDatastore: AppDatastore,
+        connectionChecker: ConnectionChecker,
+        indexHolder: IndexHolder
+    ): Repository =
+        RepositoryImpl(exploreApi, appDatabase, appDatastore, connectionChecker, indexHolder)
+}
