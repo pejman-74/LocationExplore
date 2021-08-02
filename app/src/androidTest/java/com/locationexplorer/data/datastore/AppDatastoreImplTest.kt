c040402e3dd548885dbebfb81f98d9a33d594853
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-08-02 17:12:29 +0430

    AppDatastoreImplTest updated.provided real data store manually.

--- app/src/androidTest/java/com/locationexplorer/data/datastore/AppDatastoreImplTest.kt
+++ app/src/androidTest/java/com/locationexplorer/data/datastore/AppDatastoreImplTest.kt
@@ -1,8 +1,10 @@
 package com.locationexplorer.data.datastore
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.platform.app.InstrumentationRegistry
 import com.google.common.truth.Truth.assertThat
 import com.locationexplorer.data.model.share.SimpleLocation
+import com.squareup.moshi.Moshi
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.CoroutineScope
@@ -26,20 +28,29 @@ class AppDatastoreImplTest {
     val instaTaskExecutorRule = InstantTaskExecutorRule()
 
     @Inject
-    lateinit var appDatastore: AppDatastore
-
-    @Inject
     lateinit var coroutineScope: CoroutineScope
 
+    @Inject
+    lateinit var moshi: Moshi
+
     private val testCoroutineScope get() = coroutineScope as TestCoroutineScope
+
+    lateinit var appDatastore: AppDatastore
 
     @Before
     fun setUp() {
         hiltAndroidRule.inject()
+        appDatastore = AppDatastoreImpl(
+            InstrumentationRegistry.getInstrumentation().targetContext.applicationContext,
+            "TEST_DATASTORE",
+            coroutineScope,
+            moshi
+        )
     }
 
     @After
     fun tearDown() {
+
         /**
          * clear data data store after each test
          * */
