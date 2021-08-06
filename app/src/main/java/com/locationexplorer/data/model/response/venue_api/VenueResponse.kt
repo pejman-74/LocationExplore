b1ec405681e74162f87088b725258771b6321a2e
Pejman Ranjbar <40461630+pejman-74@users.noreply.github.com>
2021-08-06 15:35:28 +0430

    VenueResponse name fixed.

--- app/src/main/java/com/locationexplorer/data/model/response/venue_api/VenueResponse.kt
+++ app/src/main/java/com/locationexplorer/data/model/response/venue_api/VenueResponse.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class VenueResoponse(
+data class VenueResponse(
     @Json(name = "meta")
     val meta: Meta?,
     @Json(name = "response")
